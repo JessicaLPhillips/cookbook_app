f3a43ca008b5758940bd9327ff09019b9b1f5403
Jessica Phillips <jlphilli22@ymail.com>
2018-10-30 09:33:15 -0400

    added recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/create'
+  get 'recipes/edit'
+  get 'recipes/update'
   devise_for :users
   # root to: "home#index"
 end
