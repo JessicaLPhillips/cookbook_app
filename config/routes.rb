51023d668d44dc67c1c7be310f6d3bc562270c50
Jessica Phillips <jlphilli22@ymail.com>
2018-10-30 09:41:16 -0400

    added groups controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'groups/new'
+  get 'groups/create'
+  get 'groups/edit'
+  get 'groups/update'
+  get 'groups/index'
+  get 'groups/show'
   devise_for :users
   # root to: "home#index"
 end
