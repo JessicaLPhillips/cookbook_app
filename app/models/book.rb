7fba91d491d550990e39f8e5a6a65fd26498ed71
Jessica Phillips <jlphilli22@ymail.com>
2018-10-29 22:32:38 -0400

    changes to book, recipe and user associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+	has_many :recipes
+	has_many :recipes, through: :book_recipes
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
 	belongs_to :user
 	belongs_to :group
 	has_many :ingredients
+	has_many :ingredients through: :recipe_ingredient
 end
