5ec99df409ad5be952dde26a5227a484c2c47553
Jessica Phillips <jlphilli22@ymail.com>
2018-11-02 07:55:39 -0400

    started books controller, updated routes

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,37 @@
+class BooksController < ApplicationController
+before_action :authenticate_user!
+
+  def index
+    @books = current_group.books.all
+  end
+
+  def show
+    @book = current_user.book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to book_path(@book), notice: "Book Created!"
+    else
+      render :new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private  
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	root to: 'home#index'
   resources :groups, except: [:update]
   resources :recipes, except: [:update]
+  resources :books
   resources :ingredients
   devise_for :users, controllers: {
     registrations: 'users/registrations', sessions: 'users/sessions'}
