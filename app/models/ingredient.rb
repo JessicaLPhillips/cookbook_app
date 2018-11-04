24e883a740ec4887858a94c76af49c8cfa76e9a2
Jessica Phillips <jlphilli22@ymail.com>
2018-11-04 11:29:10 -0500

    worked on partial form and styling on home page

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,3 +2,7 @@ class Ingredient < ApplicationRecord
 	has_many :recipe_ingredients
 	has_many :recipes, through: :recipe_ingredients
 end
+
+def ingredients
+	"#{recipe_ingredients.quantity} #{recipe_ingredients.unit} #{ingredients.name}"
+end
